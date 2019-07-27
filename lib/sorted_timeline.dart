eaa23fb7c45f45fd1802a42596bad7bf534b7a09
onatcipli <onatcipli@outlook.com>
2019-07-27 17:08:27 +0300

    Add SortedTimeline that takes DrawTimeline as a painter

--- lib/sorted_timeline.dart
+++ lib/sorted_timeline.dart
@@ -1,5 +1,39 @@
 library sorted_timeline;
 
-class SortedTimeline{
+import 'package:flutter/material.dart';
+import 'package:sorted_timeline/draw_timeline.dart';
 
+class SortedTimeline extends StatefulWidget {
+  @override
+  _SortedTimelineState createState() => _SortedTimelineState();
+}
+
+class _SortedTimelineState extends State<SortedTimeline> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CustomPaint(
+        painter: DrawTimeline(),
+        child: Container(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: <Widget>[Text('1')],
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Column(
+                  children: <Widget>[Text('Card')],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
