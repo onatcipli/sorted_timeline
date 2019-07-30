6a32e834d2c6e4d937a6123423e0db7cadfa4828
onatcipli <onatcipli@outlook.com>
2019-07-30 21:26:37 +0300

    Update SortedTimeline => SortOption, List of TimelineItems

--- lib/sorted_timeline.dart
+++ lib/sorted_timeline.dart
@@ -2,13 +2,28 @@ library sorted_timeline;
 
 import 'package:flutter/material.dart';
 import 'package:sorted_timeline/draw_timeline.dart';
+import 'package:sorted_timeline/timeline_item.dart';
+
+enum SortOption { NewToOld, OldToNew }
 
 class SortedTimeline extends StatefulWidget {
+  final List<TimelineItem> timelineItems;
+  final SortOption sortOption;
+
+  SortedTimeline({
+    @required this.timelineItems,
+    this.sortOption = SortOption.NewToOld,
+  }) : assert(timelineItems != null);
+
   @override
   _SortedTimelineState createState() => _SortedTimelineState();
 }
 
 class _SortedTimelineState extends State<SortedTimeline> {
+  List<TimelineItem> get timelineItems => widget.timelineItems;
+
+  SortOption get sortOption => widget.sortOption;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,18 +31,21 @@ class _SortedTimelineState extends State<SortedTimeline> {
         painter: DrawTimeline(),
         child: Container(
           child: Row(
-            mainAxisSize: MainAxisSize.max,
             children: <Widget>[
               Expanded(
                 flex: 1,
                 child: Column(
-                  children: <Widget>[Text('1')],
+                  children: timelineItems.map((TimelineItem timelineItem) {
+                    return Icon(Icons.arrow_right);
+                  }).toList(),
                 ),
               ),
               Expanded(
                 flex: 3,
                 child: Column(
-                  children: <Widget>[Text('Card')],
+                  children: timelineItems.map((TimelineItem timelineItem) {
+                    return timelineItem.itemBuilder(context);
+                  }).toList(),
                 ),
               ),
             ],
